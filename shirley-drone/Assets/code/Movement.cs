2dea6949d4575abcf240d83ce7af442905f419fa
heyssshhh <xinyi.zhang.19@dartmouth.edu>
2018-09-25 22:29:56 -0400

    drones

---
+++ shirley-drone/Assets/code/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+
+    //destination marker reference
+    public GameObject markerGoal;
+    //parent position
+    Vector3 parentPos;
+    //navmesh component reference
+    UnityEngine.AI.NavMeshAgent agent;
+
+
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void MoveToTarget()
+    {
+        if (markerGoal.activeInHierarchy)
+        {
+            parentPos = markerGoal.transform.parent.position;
+            agent.SetDestination(parentPos);
+        }
+    }
+
+
+    private void Update()
+    {
+        MoveToTarget();
+    }
+}
